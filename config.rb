34da0c666de69adb37f239d6610bc3e7497d4ae1
mo-zag <mo_zag@yahoo.co.uk>
2010-09-29 16:57:42 +0100

    For users to view

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
 # Folder to write PHP files to must be full path to directory
-@folderTo ="E:/business/test/php"
+@folderTo ="C:/location-of-php-folder"
 
 # Location to write SASS files converted to css must be full path to directory
-@cssFolderTo = "E:/business/test/php/stylesheets"
+@cssFolderTo = "C:/location-to-save-converted-sass-files"
 
 # Folder to watch for changes
 @dirFolderTo = "haml-sass"
