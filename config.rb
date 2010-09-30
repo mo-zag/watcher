d4cd0be114040ea32659b42a569357bb0acd515d
mo-zag <mo_zag@yahoo.co.uk>
2010-09-30 17:30:09 +0100

    Updated config file

--- config.rb
+++ config.rb
@@ -1,13 +1,14 @@
 # Folder to write PHP files to must be full path to directory
-@folderTo ="E:/business/test/php"
+@folderTo ="C:/location-of-php-folder"
 
 # Location to write SASS files converted to css must be full path to directory
-@cssFolderTo = "E:/business/test/php/stylesheets"
+@cssFolderTo = "C:/location-to-save-converted-sass-files"
 
 # Folder to watch for changes
 @dirFolderTo = "haml-sass"
 
 #Sass options
 @sassoptions = { :style => :compact, :load_paths => ['haml-sass/stylesheets'], :template_location => '.', :cache => true, :cache_location => './.sass-cache', :syntax => :sass}
+
 #Haml options
 @hamloptions = {:attr_wrapper => '"'}
